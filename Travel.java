ce22f2a1616040cc90a3a3e12cbba360c3df7abc
richard-soctab <richard.clarke@socialtables.com>
2019-03-12 17:21:41 -0400

    Change of plans Now each if statement for locating the address also executes a block of code responsible for removing the address from the list r. The start and end outcomes are already written.

--- Travel.java
+++ Travel.java
@@ -37,21 +37,24 @@ Confirm r contains State/Zipcode
    Middle
     The zipcode is neither at the start or end of the list
    
-  Get address
+  Get address and remove from list r
    Start 1
     Use indexof method to find first comma
     Take a substring from 0 to the comma
-  Extract the address
+    list = Substring starting at the end of the address + 1
    End 3
     Lastindexof to find the last comma 
     Take a substring from the comma to r.length() - 1
-   Middle 2 
+    list = Substring from 0 to the instance of the address
+   *****    
+    Middle 2 
     Take a substring from 0 to the zipcode
-    Recycle code from End 
-*****    
-  Update list r without the address
+    Recycle code from End
+
+
+  
    Start
-    Substring starting at the end of the address + 1
+    
    End
     Substring from 0 to the start of the address
    Middle
@@ -81,7 +84,7 @@ Confirm r contains State/Zipcode
 public class Travel { 
  public static void main (String[] args) { 
   String addresses = "123 Main Street St. Louisville OH 43072,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432";
-  String zipcode = "OH 43072";
+  String zipcode = "NY 56432";
   System.out.println(travel(addresses, zipcode));
   //JUST GOOFIN AROUND
  } 
@@ -95,21 +98,24 @@ public class Travel {
    if (r.contains(zipcode)) {
     String address = "";
 
-  //See if the address is at the beginning of the list
+  //See if the address is at the beginning of the list and remove the first address
     if (r.indexOf(zipcode) == r.indexOf(",") - 8){
       address = getAddress(1, r, zipcode);
+      r = r.substring(address.length() + 1);
     }
-  //See if the address is at the end of the list
+  //See if the address is at the end of the list and remove the last address
     else if ((r.indexOf(zipcode) == r.length() - 8) && (r.charAt(r.length() - 1) != ',')){
       address = getAddress(3, r, zipcode);
+      r = r.substring(0, (r.indexOf(address) - 1));
     }
-  //The address must be in the middle of the list
+  //See if the address is in the middle of the list and remove it
     else{
       address = getAddress(2, r, zipcode);
+    
     }
 
   //TEST: Display the list r without the address
-    System.out.println(r.substring(address.length() + 1));
+    System.out.println(r);
 
   //Get the street number of the address
     String streetNumber = getStreetNumber(address);
@@ -139,8 +145,9 @@ public class Travel {
 
     case(3): commaIndex = list.lastIndexOf(",");
              address = list.substring(++commaIndex, list.length());
+
    }
-  
+   
   return address;
  }
 
