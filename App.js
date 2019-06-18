442b44fdffb5cbb5e4232a0b85a2679945c9bd00
richard-remote <clarkeril8082@gmail.com>
2019-06-18 13:44:36 -0400

    Linking routes Installed react-navigation and react-native-gesture-handler //        - Created two Components: HomeScreen and ViewList //        - Created a stack navigator with two routes: HomeScreen, and ViewList //        - Set the home route to the HomeScreen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Alert, AppRegistry, BackHandler, Button, Platform, StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
 
 //5/22/19 - No clue what these are just yet. I'll keep em commented out
 //          I added flexboxes to display layouts for the exit link, title, and links
@@ -8,50 +9,68 @@ import {Alert, AppRegistry, BackHandler, Button, Platform, StyleSheet, Text, Vie
 //5/31/19 - I added bold, black styled text for the title
 
 //6/13/19 - Aligned the Title and View List option and removed the exit button
-//import {createStackNavigator, createAppContainer} from 'react-navigation';
+//6/18/19 - Installed react-navigation and react-native-gesture-handler
+//        - Created two Components: HomeScreen and ViewList
+//        - Created a stack navigator with two routes: HomeScreen, and ViewList
+//        - Set the home route to the HomeScreen
+//        - 
 
-
-export default class App extends Component {
-  _onPressButton(){
-    BackHandler.exitApp();
-  }
-
+/**Home Screen Component */
+class HomeScreen extends Component {
   render() {
     return (
-      /**ooooooo */
+      /*View/Text Components for the Title (Salesman Travel)
+        View/Text Components for View/Search list options  */
 
       <View style= {{flex: 1}}>
         <View style= {{flex: 1, flexDirection: 'row', backgroundColor: 'yellow'}}>
-          
-
           <Text style={{fontSize: 50, fontWeight: 'bold', color: 'black', textAlign: "center"}}>Salesman's Travel</Text>
         </View>
 
-        <View style= {{flex: 2, backgroundColor: 'yellow'}}>
-          
-          <Text style={{fontSize: 30, fontWeight: 'bold', color: 'green', textAlign: "center"}}>View List</Text>
+        <View style= {{flex: 3, backgroundColor: 'yellow'}}>
+          <Text style={{fontSize: 30, fontWeight: 'bold', color: 'green', textAlign: "center"}}
+            onPress= {()=> this.props.navigation.navigate('ViewList')}>View List</Text>
           <Text style={{fontSize: 30, fontWeight: 'bold', color: 'green', textAlign: 'center'}}>Search List</Text>
         </View>
-        <View style= {{flex: 2, backgroundColor: 'yellow'}}></View>
       </View>
       
     );
   }
 }
 
+/**View List Component */
+class ViewList extends Component{
+  render(){
+    return(
+      <View></View>
+    );
+  }
+}
+
+//Exports the root stack navigator
+//Two routes: Home and Details
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    ViewList: ViewList
+  },
+  {
+    backgroundColor: '#FFFF00',
+    initalRouteName: 'Home'
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component{
+  render(){
+    return <AppContainer />;
+  }
+}
+
 /* 5/22/19 - A whooooole lotta mess down here. I'll clean it up as I go along.
              For now, I just want the bare bones skeleton
 export default createAppContainer(AppNavigator);
-
-
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 
 
 
