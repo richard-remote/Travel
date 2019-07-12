d453308dc1d2d3904892f7a2e1038062e471fcea
codingfanatic <lclarkerichard@yahoo.com>
2019-07-11 22:34:32 -0400

    Unfinished changes to the MainActivity *Removed the Search List option from the TextView. It should have its own TextView so it can call  the onClick() function independently.

--- app/src/main/java/com/example/travel/MainActivity.java
+++ app/src/main/java/com/example/travel/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.travel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View; //View object will be used to obtain the text of the optoin selected
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Atare(View view){
+        String option_text;
+
     }
 }
