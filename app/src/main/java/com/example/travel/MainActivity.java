f1fdd45919fb4c3e2ef0adb0a3113d92b219ff3b
codingfanatic <lclarkerichard@yahoo.com>
2019-07-21 16:40:39 -0400

    Updated Features Removed the Title TestView Added the action bar with a black background Added classes for ListAddresses and SearchAddresses Configured both classes Wrote the onClick method called press Added the press method to the activity_main.xml file Added color to both TextViews to identify clickable areas Changed the ActionBar titles of the child Activities Set the parentActivity in the AndroidManifest.xml Styled the child Activity backgrounds to #000000

--- app/src/main/java/com/example/travel/MainActivity.java
+++ app/src/main/java/com/example/travel/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.travel;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View; //View object will be used to obtain the text of the optoin selected
+import android.widget.TextView;
+import android.view.View;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Atare(View view){
-        String option_text;
+    public void press(View View){
+        //Cast the View object as a TextView
+        //Use the getText() getter method to obtain the text in the TextView
+        //Use the toString() method to return the text as a String
+        //If the String equals "View List", create an Intent for starting the ListAddresses Activity
+        //Else, if the String equals "Search List", create an Intent for starting the SearchAddresses Activity
+        
+        text = ((TextView) View).getText().toString();
 
+        if (text.equals("View List")) {
+            //Create an Intent using the ListAddresses class
+            intent = new Intent(this, ListAddresses.class);
+            startActivity(intent);
+        }
+
+        else if (text.equals("Search List")) {
+            //Create an Intent using the SearchAddresses class
+            intent = new Intent(this, SearchAddresses.class);
+            startActivity(intent);
+        }
     }
 }
